d21a1d04b434e05ea10a333878b45491ef27d812
Miguel Angel <mangelladen@gmail.com>
2021-06-21 00:05:11 -0500

    :tada: Initializing repository

---
+++ service-product/src/test/java/spring/microservice/serviceproduct/ProductServiceMockTest.java
@@ -0,0 +1,59 @@
+package spring.microservice.serviceproduct;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import spring.microservice.serviceproduct.entity.Category;
+import spring.microservice.serviceproduct.entity.Product;
+import spring.microservice.serviceproduct.repository.ProductRepository;
+import spring.microservice.serviceproduct.service.ProductService;
+import spring.microservice.serviceproduct.service.ProductServiceImpl;
+
+@SpringBootTest
+public class ProductServiceMockTest {
+   
+   @Mock
+   private ProductRepository productRepository;
+
+   private ProductService productService;
+
+   @BeforeEach
+   public void setup(){
+      MockitoAnnotations.openMocks(this);
+      productService = new ProductServiceImpl(productRepository);
+
+      Product computer = Product.builder()
+         .id(1L)
+         .name("computer")
+         .category(Category.builder().id(1L).build())
+         .price(Double.parseDouble("12.5"))
+         .stock(Double.parseDouble("5"))
+         .build();
+
+      Mockito.when(productRepository.findById(1L))
+         .thenReturn(Optional.of(computer));
+
+      Mockito.when(productRepository.save(computer))
+         .thenReturn(computer);
+   }
+
+   @Test
+   public void whenValidGetID_ThenReturnProduct(){
+      Product found = productService.getProduct(1L);
+      Assertions.assertThat(found.getName()).isEqualTo("computer");
+   }
+
+   @Test
+   public void whenValidUpdateStock_ThenReturnNewStock(){
+      Product newStock = productService.updateStock(1L, Double.parseDouble("8"));
+      Assertions.assertThat(newStock.getStock()).isEqualTo(13);
+   }
+
+}
